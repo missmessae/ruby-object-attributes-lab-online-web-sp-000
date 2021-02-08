95cccf3857688384e7179aae930364789c73ef54
Annie Wiegel <annewiegel17@gmail.com>
2021-02-08 20:59:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,14 @@ class Dog
   end
 
   def name
-    @dog_name    
+    @dog_name
   end
 
   def breed=(dog_breed)
-    @breed = dog_breed   
+    @breed = dog_breed
   end
 
   def breed
-    @breed    
+    @breed
   end
 end
