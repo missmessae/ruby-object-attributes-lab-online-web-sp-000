931dde5175e7e1767a5057c3c23489e774ca8ebf
Annie Wiegel <annewiegel17@gmail.com>
2021-02-08 21:01:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(new_person)
+    @name = new_person
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    @job
+  end
+end
