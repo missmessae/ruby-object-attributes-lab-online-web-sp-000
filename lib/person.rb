7880e455d1c0cba14d4700acf33b365667877b83
Annie Wiegel <annewiegel17@gmail.com>
2021-02-08 20:59:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,17 @@
 class Person
-  def method_name
-  
+  def name=(new_person)
+    @name = new_person
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
   end
 end
